730d3622f5383c6aca36b148f538ac71a755d8bb
Pudding1989 <budding1989@outlook.com>
2021-08-16 06:27:26 +0800

    refactor: 設定 handlebars副檔名為「.hbs」 BREAKING CHANGE: 資料移至 Mongoose資料庫

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const port = 3000
 
 //載入模板引擎
 const exphbs = require('express-handlebars')
-app.set('view engine', 'handlebars')
-// 設定layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+// 設定預設layout，設定副檔名為「hbs」
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 
 //載入靜態檔案
 app.use(express.static('public'))
-
-//載入清單餐廳JSON資料
-const restaurantData = require('./restaurant.json')
 
 // 載入mongoose
 const mongoose = require('mongoose')
@@ -26,29 +23,37 @@ dataBase.once('open', () => {
   console.log('MongoDB Connected  ｡:.ﾟヽ(*´∀`)ﾉﾟ.:｡ ')
 })
 
-//預設路由
+//載入model
+const Restaurant = require('./models/restaurant')
+
+//首頁路由
 app.get('/', (req, res) => {
-  res.render('index', { alt: '我的餐廳清單', data: restaurantData.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurantData => res.render('index', { alt: '我的餐廳清單', restaurantData }))
+    .catch(error => console.error(error))
 })
+
+
 
 //個別內頁路由
-app.get('/restaurants/:id', (req, res) => {
-  const userSelect = restaurantData.results.find(object => object.id.toString()
-    === req.params.id)
-  res.render('show', { alt: '好。吃', object: userSelect })
-})
+// app.get('/restaurants/:id', (req, res) => {
+//   const userSelect = restaurantData.results.find(object => object.id.toString()
+//     === req.params.id)
+//   res.render('show', { alt: '好。吃', object: userSelect })
+// })
 
 //搜尋結果路由
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
-  let invalidClass = ''
-  if (!keyword.length) invalidClass = "is-invalid"
-  //先搜尋店名再搜尋種類
-  const result = restaurantData.results.filter(object => {
-    return object.name.toLowerCase().includes(keyword.toLowerCase()) || object.category.includes(keyword)
-  })
-  res.render('index', { alt: '我的餐廳清單', keyword, invalidClass, data: result})
-})
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword.trim()
+//   let invalidClass = ''
+//   if (!keyword.length) invalidClass = "is-invalid"
+//   //先搜尋店名再搜尋種類
+//   const result = restaurantData.results.filter(object => {
+//     return object.name.toLowerCase().includes(keyword.toLowerCase()) || object.category.includes(keyword)
+//   })
+//   res.render('index', { alt: '我的餐廳清單', keyword, invalidClass, data: result })
+// })
 
 //監聽伺服器
 app.listen(port, () => {
